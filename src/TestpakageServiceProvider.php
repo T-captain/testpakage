854c51df40bfcf38d8f6d64f65aa9381d2cf678f
T-Captain <165770539+T-captain@users.noreply.github.com>
2025-01-12 00:17:54 +0800

    Update TestpakageServiceProvider.php

--- src/TestpakageServiceProvider.php
+++ src/TestpakageServiceProvider.php
@@ -6,21 +6,40 @@ use Illuminate\Support\ServiceProvider;
 
 class YourPackageNameServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'yourpackagename');
-        $this->publishes([
-            __DIR__.'/../config/yourpackagename.php' => config_path('yourpackagename.php'),
-        ]);
-    }
-
     public function register()
     {
-        
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/yourpackagename.php', 'yourpackagename'
-        );
+        // 这里可以注册任何需要的服务绑定或配置
+    }
+
+    public function boot()
+    {
+        // 如果您有命令需要注册，可以在这里注册
+        // if ($this->app->runningInConsole()) {
+        //     $this->commands([
+        //         \YourVendor\YourPackageName\Commands\YourCommand::class,
+        //     ]);
+        // }
+
+        // 加载路由 - 保留此部分以便将来使用
+        // if (file_exists($routes = __DIR__.'/../routes/web.php')) {
+        //     $this->loadRoutesFrom($routes);
+        // }
+
+        // 加载数据库迁移 - 保留此部分以便将来使用
+        // if (is_dir($migrations = __DIR__.'/../database/migrations')) {
+        //     $this->loadMigrationsFrom($migrations);
+        // }
+
+        // 加载翻译文件 - 保留此部分以便将来使用
+        // if (is_dir($lang = __DIR__.'/../resources/lang')) {
+        //     $this->loadTranslationsFrom($lang, 'yourpackagename');
+        // }
+
+        // 发布配置文件 - 保留此部分以便将来使用
+        // if (file_exists($config = __DIR__.'/../config/yourpackagename.php')) {
+        //     $this->publishes([
+        //         $config => config_path('yourpackagename.php'),
+        //     ]);
+        // }
     }
 }
